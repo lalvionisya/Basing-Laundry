9cc21cb31b49fa18510b03fd94c82f61fb876aa1
lidia <104456433+lalvionisya@users.noreply.github.com>
2023-05-17 16:42:59 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,107 @@
+// image carousel data
+const images = [
+    "https://accurate.id/wp-content/uploads/2022/08/accurate.id-Berbagai-Perlengkapan-Laundry-yang-Harus-Dipersiapkan.jpg",
+    "https://lokalsupportlokal.id/wp-content/uploads/2020/11/usaha-laundry-kiloan-untuk-pemula-usaha-laundry-kiloan-di-rumah-usaha-laundry-kiloan-dengan-modal-kecil-usaha-laundry-kiloan-franchise-626x385.jpg",
+  ];
+  
+  // view prop types error -> code to paste
+  // get ColorPropType(): $FlowFixMe {
+  //     return require('deprecated-react-native-prop-types').ColorPropType;
+  //   },
+  
+  //   get EdgeInsetsPropType(): $FlowFixMe {
+  //     return require('deprecated-react-native-prop-types').EdgeInsetsPropType;
+  //   },
+  
+  //   get PointPropType(): $FlowFixMe {
+  //     return require('deprecated-react-native-prop-types').PointPropType;
+  //   },
+  
+  //   get ViewPropTypes(): $FlowFixMe {
+  //     return require('deprecated-react-native-prop-types').ViewPropTypes;
+  //   },
+  
+  // services data code
+  const services = [
+    {
+      id: "0",
+      image: "https://cdn-icons-png.flaticon.com/128/3003/3003984.png",
+      name: "Cuci & Setrika",
+    },
+    {
+      id: "11",
+      image: "https://cdn-icons-png.flaticon.com/128/2975/2975175.png",
+      name: "Cuci Aja",
+    },
+    {
+      id: "12",
+      image: "https://cdn-icons-png.flaticon.com/128/9753/9753675.png",
+      name: "Setrika Aja",
+    },
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/995/995016.png",
+      name: "Cuci Satuan",
+    },
+  ];
+  
+  // products1 data 
+  const services1 = [
+      {
+        id: "0",
+        image: "https://cdn-icons-png.flaticon.com/128/3003/3003984.png",
+        name: "Cuci & Setrika /Kg",
+        quantity: 0,
+        price: 5000,
+      },
+      {
+        id: "11",
+        image: "https://cdn-icons-png.flaticon.com/128/2975/2975175.png",
+        name: "Cuci Aja /Kg",
+        quantity: 0,
+        price: 3500,
+      },
+      {
+        id: "12",
+        image: "https://cdn-icons-png.flaticon.com/128/9753/9753675.png",
+        name: "Setrika Aja /Kg",
+        quantity: 0,
+        price: 3500,
+      },
+
+      
+    ];
+
+     // products2 data 
+  const services2 = [
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/4643/4643574.png",
+      name: "Kemeja /Pcs",
+      quantity: 0,
+      price: 4000,
+    },
+    {
+      id: "14",
+      image: "https://cdn-icons-png.flaticon.com/128/892/892458.png",
+      name: "Kaos /Pcs",
+      quantity: 0,
+      price: 3000,
+    },
+    {
+      id: "15",
+      image: "https://cdn-icons-png.flaticon.com/128/9609/9609161.png",
+      name: "Gaun /Pcs",
+      quantity: 0,
+      price: 10000,
+    },
+    {
+      id: "16",
+      image: "https://cdn-icons-png.flaticon.com/128/599/599388.png",
+      name: "Celana Jeans /Pcs",
+      quantity: 0,
+      price: 8000,
+    },
+
+    
+  ];
